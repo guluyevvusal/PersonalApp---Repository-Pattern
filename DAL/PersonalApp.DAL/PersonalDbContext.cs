a2c90a244ff5f2ead35e9b15f2f4ab7661a36bd7
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-08-12 14:57:55 +0400

    Repository, DbContext and Entity created

---
+++ DAL/PersonalApp.DAL/PersonalDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalApp.Domains.Entities;
+
+namespace PersonalApp.DAL
+{
+    public class PersonalDbContext:DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Personal> Personals { get; set; }
+
+    }
+}
