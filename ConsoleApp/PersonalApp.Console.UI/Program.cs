1a8f4837643884115373e453e22e52b631c8d4eb
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-08-12 15:40:02 +0400

    Unit of work created

---
+++ ConsoleApp/PersonalApp.Console.UI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalApp.Console.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            UnitOfWork unitofwork = new UnitOfWork(new PersonalDbContext());
+           
+            unitofwork.DepartmentRepository.Add(new Department()
+            {
+                Name = "Engineer",
+                isActive = true,
+                CreatedDate = DateTime.Now
+            });
+           
+            unitofwork.Department.Repository.Add(new Department() { Name = "Developer", IsActive = true, CreatedDate = DateTime.Now });
+           
+            unitofwork.Complete();
+        }
+    }
+}
+
---
+++ DAL/PersonalApp.DAL/UnitofWork/IUnitofWork.cs
@@ -0,0 +1,17 @@
+﻿using PersonalApp.DAL.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalApp.DAL.UnitofWork
+{
+    public interface IUnitofWork:IDisposable
+    {
+        IDeparmentRepository DeparmentRepository { get; }
+        IPersonalRepository PersonalRepository { get; }
+
+        int Complete();
+    }
+}
