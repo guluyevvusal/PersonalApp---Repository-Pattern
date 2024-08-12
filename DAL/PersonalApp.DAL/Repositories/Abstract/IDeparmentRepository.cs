58f8f11363b83eb5628b36567e05398eadfc1fd4
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-08-12 15:13:41 +0400

    Repositors were implemented

---
+++ DAL/PersonalApp.DAL/Repositories/Abstract/IDeparmentRepository.cs
@@ -0,0 +1,15 @@
+﻿using PersonalApp.Domains.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalApp.DAL.Repositories.Abstract
+{
+    public interface IDeparmentRepository:IRepository<Department>
+    {
+        IEnumerable<Department> GetTopDepartmnets(int count);
+        IEnumerable<Department> GetDepartmentsWithPersonals();
+    }
+}
---
+++ DAL/PersonalApp.DAL/Repositories/Concrete/DepartmentRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalApp.DAL.Repositories.Abstract;
+using PersonalApp.Domains.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalApp.DAL.Repositories.Concrete
+{
+    public class DepartmentRepository : Repository<Department>, IDeparmentRepository
+    {
+        public DepartmentRepository(PersonalDbContext context) : base(context)
+        {
+
+        }
+
+        public IEnumerable<Department> GetDepartmentsWithPersonals()
+        {
+            return PersonalDbContext.Departments.Include("Personals").ToList();
+        }
+
+        public IEnumerable<Department> GetTopDepartmnets(int count)
+        {
+            return PersonalDbContext.Departments.Take(count);
+        }
+
+
+        public PersonalDbContext PersonalDbContext { get { return _dbcontext as PersonalDbContext; } }
+    }
+}
